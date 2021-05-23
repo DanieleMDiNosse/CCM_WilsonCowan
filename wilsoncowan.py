751333eefe56446054164151ec3420e0ed754b52
Daniele Maria <danielemdinosse@gmail.com>
2021-05-23 18:14:38 +0200

    added several other causation interaction

--- wilsoncowan.py
+++ wilsoncowan.py
@@ -59,6 +59,21 @@ def power_spectrum(x, t, step):
 
     return X
 
+def prediction_skill(x, y, lag, embed):
+    x_emb, y_emb = Embed(x), Embed(y)
+    x_emb = x_emb.embed_vectors_1d(lag, embed)
+    y_emb = y_emb.embed_vectors_1d(lag, embed)
+
+    x1tr, x1te, x2tr, x2te = train_test_split(x_emb, y_emb, percent=.75)
+
+    CCM = ccm.CCM()
+    len_tr = len(x1tr)
+    lib_lens = np.arange(10, len_tr, len_tr/20, dtype='int')
+    CCM.fit(x1tr, x2tr)
+    x1p, x2p = CCM.predict(x1te, x2te, lib_lengths=lib_lens)
+    sc1, sc2 = CCM.score()
+    return sc1, sc2, x_emb, y_emb
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -119,7 +134,6 @@ if __name__ == "__main__":
         alpha, beta = (1.3, -2.0)
         logging.info(f'alpha, beta = ({alpha}, {beta})')
 
-
     # initial conditions
     x0 = [0.1, 0.1, 0.1, 0.1]
     step = 0.1
@@ -128,7 +142,6 @@ if __name__ == "__main__":
 
     x = odeint(coupled_wilson_cowan, x0, t, args=(alpha, beta,
         k_e, k_i, c1, c2, c3, c4, tau_e, tau_i, P1, P, Pp, direction))
-
 
     E1 = x[:, 0]
     I1 = x[:, 1]
@@ -232,61 +245,93 @@ if __name__ == "__main__":
     if args.embedding:
         lag = int(input('Select lag value (first minimum on MI): '))
         embed_list = np.arange(1,10,1)
-        bestX2X1 = []
-        bestX1X2 = []
+        bestE2E1, bestE1E2 = [], []
+        bestI1I2, bestI2I1 = [], []
+        bestE1I1, bestI1E1 = [], []
+        bestE2I1, bestI1E2 = [], []
+        bestE1I2, bestI2E1 = [], []
+        bestE2I2, bestI2E2 = [], []
         for embed in embed_list:
-            X1 = e1.embed_vectors_1d(lag, embed)
-            X2 = e3.embed_vectors_1d(lag, embed)
-            #split the embedded time series
-            x1tr, x1te, x2tr, x2te = train_test_split(X1, X2, percent=.75)
+            sc1ee, sc2ee, E1_emb, E2_emb = prediction_skill(E1, E2, lag, embed)
+            bestE2E1.append(sc1ee[-1])
+            bestE1E2.append(sc2ee[-1])
 
-            CCM = ccm.CCM()
+            sc1ei, sc2ei, E1_emb, I1_emb = prediction_skill(E1, I1, lag, embed)
+            bestI1E1.append(sc1ei[-1])
+            bestE1I1.append(sc2ei[-1])
 
-            #library lengths to test
-            len_tr = len(x1tr)
-            lib_lens = np.arange(10, len_tr, len_tr/20, dtype='int')
-            #test causation
-            CCM.fit(x1tr, x2tr)
-            x1p, x2p = CCM.predict(x1te, x2te, lib_lengths=lib_lens)
+            sc1ei1, sc2ei1, E1_emb, I2_emb = prediction_skill(E1, I2, lag, embed)
+            bestI2E1.append(sc1ei1[-1])
+            bestE1I2.append(sc2ei1[-1])
 
-            sc1, sc2 = CCM.score()
-            bestX2X1.append(sc1[-1])
-            bestX1X2.append(sc2[-1])
+            sc1ei2, sc2ei2, E2_emb, I1_emb = prediction_skill(E2, I1, lag, embed)
+            bestI1E2.append(sc1ei2[-1])
+            bestE2I1.append(sc2ei2[-1])
+
+            sc1ei3, sc2ei3, E2_emb, I2_emb = prediction_skill(E2, I2, lag, embed)
+            bestI2E2.append(sc1ei3[-1])
+            bestE2I2.append(sc2ei3[-1])
+
+            sc1ii, sc2ii, E2_emb, I1_emb = prediction_skill(I1, I2, lag, embed)
+            bestI2I1.append(sc1ii[-1])
+            bestI1I2.append(sc2ii[-1])
 
         plt.figure()
         plt.title('Prediction skills as function of embedding dimension')
-        plt.plot(embed_list, bestX2X1, label="X2 for X1")
-        plt.plot(embed_list, bestX1X2, label="X1 for X2")
+        plt.plot(embed_list, bestE2E1, label="E2 for E1")
+        plt.plot(embed_list, bestE1E2, label="E1 for E2")
+        plt.plot(embed_list, bestI1I2, label="I1 for I2")
+        plt.plot(embed_list, bestI2I1, label="I2 for I1")
+        plt.plot(embed_list, bestE1I1, label="E1 for I1")
+        plt.plot(embed_list, bestI1E1, label="I1 for E1")
+        plt.plot(embed_list, bestE2I1, label="E2 for I1")
+        plt.plot(embed_list, bestI1E2, label="I1 for E2")
+        plt.plot(embed_list, bestE1I2, label="E1 for I2")
+        plt.plot(embed_list, bestI2E1, label="I2 for E1")
+        plt.plot(embed_list, bestI2E2, label="I2 for E2")
+        plt.plot(embed_list, bestE2I2, label="E2 for I2")
         plt.grid()
         plt.legend()
         plt.show()
 
         embed = int(input('Choose dimension of the embedding space: '))
-        X1 = e1.embed_vectors_1d(lag, embed)
-        X2 = e3.embed_vectors_1d(lag, embed)
+        sc1ee, sc2ee, E1emb, E2emb = prediction_skill(E1, E2, lag, embed)
+        sc1ii, sc2ii, I1emb, I2emb = prediction_skill(I1, I2, lag, embed)
 
         fig = plt.figure()
-        ax = fig.add_subplot(1, 2, 1, projection='3d')
-        ax.scatter(X1[:, 0], X1[:, 1], X1[:, 2], s=0.2)
-        ax.set_xlabel('E_1(t)')
-        ax.set_ylabel(f'E_1(t+{lag})')
-        ax.set_zlabel(f'E_1(t+2*{lag})')
+        plt.title(f'Embedded attractors. Dimension equal to {embed}')
+        ax = fig.add_subplot(2, 2, 1, projection='3d')
+        ax.scatter(E1emb[:, 0], E1emb[:, 1], E1emb[:, 2], s=0.2)
+        ax.set_xlabel('E1(t)')
+        ax.set_ylabel(f'E1(t+{lag})')
+        ax.set_zlabel(f'E1(t+2*{lag})')
 
-        ax = fig.add_subplot(1, 2, 2, projection='3d')
-        ax.scatter(X2[:, 0], X2[:, 1], X2[:, 2], s=0.2)
-        ax.set_xlabel('E_2(t)')
-        ax.set_ylabel(f'E_2(t+{lag})')
-        ax.set_zlabel(f'E_2(t+2*{lag})')
-        fig.suptitle(f'Embedding with dim {embed}')
+        ax = fig.add_subplot(2, 2, 2, projection='3d')
+        ax.scatter(E2emb[:, 0], E2emb[:, 1], E2emb[:, 2], s=0.2)
+        ax.set_xlabel('E2(t)')
+        ax.set_ylabel(f'E2(t+{lag})')
+        ax.set_zlabel(f'E2(t+2*{lag})')
 
-        sc1, sc2 = CCM.score()
+        ax = fig.add_subplot(2, 2, 3, projection='3d')
+        ax.scatter(I1emb[:, 0], I1emb[:, 1], I1emb[:, 2], s=0.2)
+        ax.set_xlabel('I1(t)')
+        ax.set_ylabel(f'I1(t+{lag})')
+        ax.set_zlabel(f'I1(t+2*{lag})')
+
+        ax = fig.add_subplot(2, 2, 4, projection='3d')
+        ax.scatter(I2emb[:, 0], I2emb[:, 1], I2emb[:, 2], s=0.2)
+        ax.set_xlabel('I2(t)')
+        ax.set_ylabel(f'I2(t+{lag})')
+        ax.set_zlabel(f'I2(t+2*{lag})')
+
         plt.figure()
         plt.title('Prediction skill as function of library lenght')
-        plt.plot(lib_lens, sc1, label=f'E2 used to predict E1')
-        plt.plot(lib_lens, sc2, label=f'E1 used to predict E2')
+        plt.plot(sc1ee, label=f'E2 used to predict E1')
+        plt.plot(sc2ee, label=f'E1 used to predict E2')
+        plt.plot(sc1ii, label=f'I1 used to predict I2')
+        plt.plot(sc2ii, label=f'I2 used to predict I1')
         plt.xlabel('Library lenght')
         plt.grid()
         plt.legend()
-        plt.title(f'Forecast skill (embedding dim {embed})')
         plt.show()
 
